717449e5d214a0d371e43cfaf228208257506ee9
Santiagocast <santiagojcastelli@gmail.com>
2022-08-28 22:22:14 -0300

    Genero entidades y relaciones de BD

--- src/main/java/com/preaceleracion/project/entity/Pelicula.java
+++ src/main/java/com/preaceleracion/project/entity/Pelicula.java
@@ -1,2 +1,36 @@
-package com.preaceleracion.project.entity;public class Pelicula {
+package com.preaceleracion.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import  javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name= "pelicula")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pelicula {
+    @Id
+    @Column(name = "id_pelicula")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPelicula;
+
+    private String tituloPelicula;
+
+    private String imagen;
+
+    private LocalDate fechaCreacion;
+
+    private Integer calificacion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_genero")
+    private Genero genero;
+
+    @ManyToMany(mappedBy = "peliculaAsociada")
+    private List<Personaje> personajesAsociados;
 }
