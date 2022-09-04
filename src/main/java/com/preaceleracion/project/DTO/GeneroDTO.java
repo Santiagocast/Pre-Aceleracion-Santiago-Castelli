f5cf1bbbf46cb70d60f274aa491b942922509392
Santiagocast <santiagojcastelli@gmail.com>
2022-09-04 20:43:47 -0300

    DTOS y primer POST

--- src/main/java/com/preaceleracion/project/DTO/GeneroDTO.java
+++ src/main/java/com/preaceleracion/project/DTO/GeneroDTO.java
@@ -1,2 +1,16 @@
-package com.preaceleracion.project.DTO;public class generoDTO {
+package com.preaceleracion.project.DTO;
+
+import com.preaceleracion.project.entity.Pelicula;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class GeneroDTO {
+    private Long idGenero;
+    private String nombreGenero;
+    private String imagen;
+    //private List<Pelicula> peliculasAsociadas;
 }
--- src/main/java/com/preaceleracion/project/Service/GeneroService.java
+++ src/main/java/com/preaceleracion/project/Service/GeneroService.java
@@ -1,2 +1,14 @@
-package com.preaceleracion.project.Service;public class GeneroService {
+package com.preaceleracion.project.Service;
+
+import com.preaceleracion.project.DTO.GeneroDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GeneroService {
+
+    public GeneroDTO save(GeneroDTO dto){
+        //Guardar Genero
+        return dto;
+
+    }
 }
